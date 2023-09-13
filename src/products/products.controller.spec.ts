133178f0fa1d7b658ebd811355a11ef5a5907ba6
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2023-09-13 12:19:30 +0100

    🧪 add tests for products controller and service

--- src/products/products.controller.spec.ts
+++ src/products/products.controller.spec.ts
@@ -1,20 +1,80 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
+import { Product } from './schemas/product.schema';
 
 describe('ProductsController', () => {
   let controller: ProductsController;
+  let service: ProductsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ProductsController],
-      providers: [ProductsService],
+      providers: [
+        {
+          provide: ProductsService,
+          useValue: {
+            findAll: jest.fn().mockResolvedValue([]),
+            findOne: jest.fn().mockResolvedValue({}),
+            create: jest.fn().mockResolvedValue({}),
+            update: jest.fn().mockResolvedValue({}),
+            remove: jest.fn().mockResolvedValue({
+              statusCode: 200,
+              message: 'Product deleted successfully.',
+            }),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<ProductsController>(ProductsController);
+    service = module.get<ProductsService>(ProductsService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should find all products', async () => {
+    expect(await controller.findAll(1, 5, '', null, null)).toEqual([]);
+    expect(service.findAll).toHaveBeenCalled();
+  });
+
+  it('should find one product', async () => {
+    const id = '1';
+    expect(await controller.findOne(id)).toEqual({});
+    expect(service.findOne).toHaveBeenCalledWith(id);
+  });
+
+  it('should create a product', async () => {
+    const product: Product = {
+      name: 'Product 1',
+      category: 'someCategory',
+      price: 100,
+      availability: false,
+    };
+    expect(await controller.create(product)).toEqual({});
+    expect(service.create).toHaveBeenCalledWith(product);
+  });
+
+  it('should update a product', async () => {
+    const id = '1';
+    const product: Product = {
+      name: 'Product 1',
+      category: 'someCategory',
+      price: 340,
+      availability: true,
+    };
+    expect(await controller.update(id, product)).toEqual({});
+    expect(service.update).toHaveBeenCalledWith(id, product);
+  });
+
+  it('should delete a product', async () => {
+    const id = '1';
+    expect(await controller.delete(id)).toEqual({
+      statusCode: 200,
+      message: 'Product deleted successfully.',
+    });
+    expect(service.remove).toHaveBeenCalledWith(id);
   });
 });
--- src/products/products.service.spec.ts
+++ src/products/products.service.spec.ts
@@ -3,16 +3,95 @@ import { ProductsService } from './products.service';
 
 describe('ProductsService', () => {
   let service: ProductsService;
+  const mockProducts = [
+    { id: '1', name: 'Product 1' },
+    { id: '2', name: 'Product 2' },
+  ];
+
+  const mockProductsService = {
+    create: jest.fn().mockImplementation((dto) =>
+      Promise.resolve({
+        id: '123',
+        ...dto,
+      }),
+    ),
+    remove: jest.fn((id) => {
+      return mockProducts.filter((p) => p.id != id);
+    }),
+    findAll: jest.fn(() => mockProducts),
+    findOne: jest.fn((id) => {
+      return mockProducts.find((p) => p.id === id);
+    }),
+    update: jest.fn((id, dto) => {
+      return { ...mockProducts.find((p) => p.id === id), ...dto };
+    }),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [ProductsService],
-    }).compile();
+    })
+      .overrideProvider(ProductsService)
+      .useValue(mockProductsService)
+      .compile();
 
     service = module.get<ProductsService>(ProductsService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should create a product', async () => {
+    const createProductDto = {
+      name: 'Product 1',
+      category: 'someCategory',
+      price: 100,
+      availability: false,
+    };
+
+    expect(await service.create(createProductDto)).toEqual({
+      id: '123',
+      name: createProductDto.name,
+      category: createProductDto.category,
+      price: createProductDto.price,
+      availability: createProductDto.availability,
+    });
+
+    expect(mockProductsService.create).toHaveBeenCalledWith(createProductDto);
+  });
+
+  it('should delete a product', () => {
+    expect(mockProductsService.remove('2')).toEqual([
+      {
+        id: '1',
+        name: 'Product 1',
+      },
+    ]);
+  });
+
+  it('should get all products', () => {
+    expect(mockProductsService.findAll()).toEqual(mockProducts);
+  });
+
+  it('should find one product', () => {
+    expect(mockProductsService.findOne('1')).toEqual({
+      id: '1',
+      name: 'Product 1',
+    });
+  });
+
+  it('should update a product', () => {
+    const updateProductDto = { name: 'Updated Product' };
+
+    expect(mockProductsService.update('1', updateProductDto)).toEqual({
+      id: '1',
+      name: updateProductDto.name,
+    });
+
+    expect(mockProductsService.update).toHaveBeenCalledWith(
+      '1',
+      updateProductDto,
+    );
   });
 });
